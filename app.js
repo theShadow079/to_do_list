b1e71159996f82e3c6e2e5cc99d1fc156b7812d6
theShadow079 <vinialex7@hotmail.com>
2024-01-17 23:00:30 -0300

    new changes

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const checkListRouter = require("./src/routes/checklist");
+const app = express();
+app.use(express.json());
+
+app.use("/checklist", checkListRouter);
+
+app.listen(3000, () => {
+  console.log("Servidor foi iniciado!");
+});
---
+++ src/routes/checklist.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  console.log("olá!");
+  res.send();
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.status(200).send(req.body);
+});
+
+router.get("/:id", (req, res) => {
+  console.log(req.params.id);
+  res.send(`ID: ${req.params.id}`);
+});
+
+module.exports = router;
